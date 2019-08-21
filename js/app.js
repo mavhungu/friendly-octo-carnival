48c39560fe8e2e13428fff5f139bdc30ac69fbd7
Ronewa <ronewamavhungu@gmail.com>
2019-08-21 15:14:55 +0200

    oh

--- js/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('node_mode/particles.js/particles.js', 'node_mode/particles.js/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
