a76cae3dfddfbb7b179c949ba8a94eaf46b40a15
mavhungu <ronewamavhungu@gmail.com>
2019-09-11 20:35:02 +0200

    changed error style

--- js/validates.js
+++ js/validates.js
@@ -8,27 +8,32 @@ function validateForm () {
     var name =  document.getElementById('name').value;
     if (name === "") {
         document.getElementById('status').innerHTML = "Name cannot be empty";
+		$('#status').addClass('status');
         return false;
     }
     var email =  document.getElementById('email').value;
     if (email === "") {
         document.getElementById('status').innerHTML = "Email cannot be empty";
+		$('#status').addClass('status');
         return false;
     } else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(!re.test(email)){
             document.getElementById('status').innerHTML = "Email format invalid";
+			$('#status').addClass('status');
             return false;
         }
     }
     var subject =  document.getElementById('subject').value;
     if (subject === "") {
         document.getElementById('status').innerHTML = "Subject cannot be empty";
+		$('#status').addClass('status');
         return false;
     }
     var message =  document.getElementById('message').value;
     if (message === "") {
         document.getElementById('status').innerHTML = "Message cannot be empty";
+		$('#status').addClass('status');
         return false;
     }
      document.getElementById('status').innerHTML = "Sending...";
@@ -59,10 +64,5 @@ function validateForm () {
             $('#status').text(jqXHR);
         }
     });
-    
-    
-       
+           
 }
-
-
-
